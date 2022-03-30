8bed255c38a421789dee68a7dfd971e59decc30b
Kate Hsuan <hpa@redhat.com>
2022-03-30 13:18:35 +0800

    FIrst commit

---
+++ common.py
@@ -0,0 +1,9 @@
+# well-known name for our program
+ECHO_BUS_NAME = 'com.stackoverflow.question_21793826.EchoService'
+
+# interfaces implemented by some objects in our program
+ECHO_INTERFACE = 'com.stackoverflow.question_21793826.EchoInterface'
+QUIT_INTERFACE = 'com.stackoverflow.question_21793826.QuitInterface'
+
+# paths to some objects in our program
+ECHO_OBJECT_PATH = '/EchoServerObject'
